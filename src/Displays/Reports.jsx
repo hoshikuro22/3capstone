e7910f4166a8cc57371641e8a9d813c20d6b8547
hoshikuro22 <superlaggerxz@gmail.com>
2023-08-30 05:40:50 +0800

    larbi

--- src/Displays/Reports.jsx
+++ src/Displays/Reports.jsx
@@ -1,54 +1,79 @@
-import  { useState } from 'react';
 
-const MonthYearPicker = () => {
-  const [selectedMonth, setSelectedMonth] = useState('');
-  const [selectedYear, setSelectedYear] = useState('');
-
-  const months = [
-    'January', 'February', 'March', 'April', 'May', 'June', 
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ];
-  const currentYear = new Date().getFullYear();
-  const years = Array.from({ length: 20 }, (_, index) => currentYear - index);
-
-  const handleMonthChange = (e) => {
-    setSelectedMonth(e.target.value);
-  };
-
-  const handleYearChange = (e) => {
-    setSelectedYear(e.target.value);
-  };
+const Reports = () => {
+  
+ 
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="space-x-4">
-        <select
-          className="border p-2 rounded"
-          value={selectedMonth}
-          onChange={handleMonthChange}
-        >
-          <option value="" disabled>Select Month</option>
-          {months.map((month, index) => (
-            <option key={index} value={month}>
-              {month} 
-            </option>
-          ))}
-        </select>
-        <select
-          className="border p-2 rounded"
-          value={selectedYear}
-          onChange={handleYearChange}
-        >
-          <option value="" disabled>Select Year</option>
-          {years.map((year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
-          ))}
-        </select>
+    <div className="duration-500  w-screen h-screen mt-8">
+      <div>
+        <h1 className="font-semibold text-2xl mb-2">The Reports: </h1>
+        <div className="border-2 border-black flex-col">
+          <div className="font-bold text-center flex flex-row items-center justify-center gap-32 mt-2">
+            <div className="">
+            
+            </div>
+            <div className="">
+              
+            </div>
+          </div>
+          <div className="font-bold text-center border-2 border-black my-10 mx-5 text-1xl sm:text-3xl overflow-auto">
+            <div className="text-2xl items-center text-center">
+              <body className="bg-gray-100">
+                <div className="container mx-auto p-6">
+                  <h1 className="text-2xl font-semibold mb-4">--</h1>
+                  <table className="table-auto w-full border-collapse border bg-white">
+                    <thead>
+                      <tr className="bg-gray-200">
+                        <th className="px-4 py-2">Department</th>
+                        <th className="px-4 py-2">Personnel</th>
+                        <th className="px-4 py-2">Category</th>
+                        <th className="px-4 py-2">Date</th>
+                        <th className="px-4 py-2">Document</th>
+                        <th className="px-4 py-2">Document Source</th>
+                        <th className="px-4 py-2">Organization</th>
+                        <th className="px-4 py-2">Address</th>
+                        <th className="px-4 py-2">Status</th>
+                        <th className="px-4 py-2">Remarks</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                       <td className="border px-4 py-2">dep1</td>{/* [place holder lng */}
+                       <td className="border px-4 py-2">pers1</td>
+                       <td className="border px-4 py-2">cat1</td>
+                       <td className="border px-4 py-2">date</td>
+                       <td className="border px-4 py-2">doc1</td>
+                       <td className="border px-4 py-2">docsor1</td>
+                       <td className="border px-4 py-2">org1</td>
+                       <td className="border px-4 py-2">address1</td>
+                       <td className="border px-4 py-2">stat1</td>
+                       <td className="border px-4 py-2">remarks1</td>
+                      </tr>
+                    </tbody>
+                    <tbody>
+                      <tr>
+                      <td className="border px-4 py-2">dep2</td>{/* [place holder lng */}
+                       <td className="border px-4 py-2">pers2</td>
+                       <td className="border px-4 py-2">cat2</td>
+                       <td className="border px-4 py-2">date</td>
+                       <td className="border px-4 py-2">doc2</td>
+                       <td className="border px-4 py-2">docsor2</td>
+                       <td className="border px-4 py-2">org2</td>
+                       <td className="border px-4 py-2">address2</td>
+                       <td className="border px-4 py-2">stat2</td>
+                       <td className="border px-4 py-2">remarks2</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </body>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default MonthYearPicker;
+    
+  export default Reports;
--- src/Displays/listOfPersonnel.jsx
+++ src/Displays/listOfPersonnel.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react';
+
 const ListOfPersonnel = () => {
-  const [selectedCommunication, setSelectedCommunication] = useState(null);
-
-  const handleOptionChange = (event) => {
-    setSelectedCommunication(event.target.value);
-  };
-
-  const communications = [
-    'AUTHORIZATION', 'HEIS DESIGNATIION/SPECIMEN', 'ENROLLMENT LIST', 'LIST OF GRADUATES', 'CAV CLAIM STUB', 'CAV TRACKER', 
-    'THESIS/DISSERTATION'
+  
+  const personnels = [ 'PERSONNEL 1' , 'PERSONNEL 2' , 'PERSONNEL 3' , 'PERSONNEL 4' , 'PERSONNEL 5'  
   ];
 
   return (
@@ -19,45 +12,49 @@ const ListOfPersonnel = () => {
           <div className="font-bold text-center flex flex-row items-center justify-center gap-32 mt-2">
             <div className="">
               <select
-                onChange={handleOptionChange}
+               
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option disabled selected>SELECTED COMMUNICATION</option>
-                {communications.map((communication, index) => (
-                  <option key={index} value={communication}>
-                    {communication}
+                <option disabled selected>SELECTED PERSONNEL</option>
+                {personnels.map((pers, index) => (
+                  <option key={index} value={pers}>
+                    {pers}
                   </option>
                 ))}
               </select>
             </div>
             <div className="">
-              <select
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option disabled selected>DATE</option>
-                <option>other dates</option>
-              </select>
+              
             </div>
           </div>
           <div className="font-bold text-center border-2 border-black my-10 mx-5 text-1xl sm:text-3xl overflow-auto">
             <div className="text-2xl items-center text-center">
               <body className="bg-gray-100">
                 <div className="container mx-auto p-6">
-                  <h1 className="text-2xl font-semibold mb-4">Communication Records according by Date</h1>
+                  <h1 className="text-2xl font-semibold mb-4">List of Personnels Records</h1>
                   <table className="table-auto w-full border-collapse border bg-white">
                     <thead>
                       <tr className="bg-gray-200">
-                        <th className="px-4 py-2">Communication Records</th>
+                        <th className="px-4 py-2">Files</th>
                         <th className="px-4 py-2">Date</th>
+                        <th className="px-4 py-2">Status</th>
+                        <th className="px-4 py-2">Remark</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td className="border px-4 py-2">
-                          as if file sa: {selectedCommunication ? selectedCommunication : 'communications'}
-                        </td>
-                        {/* [place holder lng */}
-                        <td className="border px-4 py-2">August-September</td>
+                       <td className="border px-4 py-2">File 1</td>{/* [place holder lng */}
+                       <td className="border px-4 py-2">date</td>
+                       <td className="border px-4 py-2">Signed</td>
+                       <td className="border px-4 py-2">good?</td>
+                      </tr>
+                    </tbody>
+                    <tbody>
+                      <tr>
+                       <td className="border px-4 py-2">File 2</td>{/* [place holder lng */}
+                       <td className="border px-4 py-2">date</td>
+                       <td className="border px-4 py-2">Pending</td>
+                       <td className="border px-4 py-2">nice?</td>
                       </tr>
                     </tbody>
                   </table>
