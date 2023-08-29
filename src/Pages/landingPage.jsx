81f0ad4bece94df0dba01f7aba5ff0a1ea6a1f9b
hoshikuro22 <superlaggerxz@gmail.com>
2023-08-30 05:47:13 +0800

    bilar

--- src/Pages/landingPage.jsx
+++ src/Pages/landingPage.jsx
@@ -1,10 +1,11 @@
-import Picture from '../assets//ched10.png'
-import { Route,Routes } from 'react-router-dom'
-import AdminPage from './AdminPage'
+
+import { Route, Routes } from 'react-router-dom';
+import AdminPage from './AdminPage';
+import Picture from '../assets/ched10.png';  
+
 function Landing() {
   return (
     <div className='bg-gray-500 h-screen'>
-     
       <div className='flex flex-col items-center justify-center p-9'>
         <div className='mt-3'>
           <div className='flex flex-col md:flex-row md:gap-6 items-center text-2xl font-semibold text-white text-center'>
@@ -17,36 +18,33 @@ function Landing() {
         </div>
       </div>
 
-      <body className="bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-lg w-80 my-10 ">
-        <h1 className="text-2xl font-semibold mb-4">User Login</h1>
-        <form>
+      <div className="bg-gray-100 flex items-center justify-center"> 
+        <div className="bg-white p-8 rounded shadow-lg w-80 my-10">
+          <h1 className="text-2xl font-semibold mb-4">User Login</h1>
+          <form>
             <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Email</label>
-                <input required type="email" id="username" name="username" className="mt-1 px-3 py-2 w-full border rounded-md focus:ring focus:ring-blue-200"></input>
+              <label className="block text-sm font-medium text-gray-700">Email</label>
+              <input required type="email" id="username" name="username" className="mt-1 px-3 py-2 w-full border rounded-md focus:ring focus:ring-blue-200" />
             </div>
             <div className="mb-4">
-                <label  className="block text-sm font-medium text-gray-700">Password</label>
-                <input required type="password" id="password" name="password" className="mt-1 px-3 py-2 w-full border rounded-md focus:ring focus:ring-blue-200"></input>
+              <label className="block text-sm font-medium text-gray-700">Password</label>
+              <input required type="password" id="password" name="password" className="mt-1 px-3 py-2 w-full border rounded-md focus:ring focus:ring-blue-200" />
             </div>
-            <button href="/admin/communication" type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">Login</button>
-        </form>
-    </div>
-    </body>
-    <Routes>
-      
-      <Route path='admin/' element={<AdminPage/>} />     {/* activated when /admin  */}
-      
-        </Routes>
-        <div className="bottom-0 absolute">
-                <a href="/admin" className="block py-1 px-2 rounded hover:bg-gray-700">(as if admin)</a>
-                <a href="/staff" className="block py-1 px-2 rounded hover:bg-gray-700">(as if staff)</a>
-             
-            </div>
+            <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">Login</button>
+          </form>
+        </div>
       </div>
 
-    
-  )
+      <Routes>
+        <Route path='/admin' element={<AdminPage />} /> 
+      </Routes>
+
+      <div className="bottom-0 absolute">
+        <a href="/admin" className="block py-1 px-2 rounded hover:bg-gray-700">(as if admin)</a>
+        <a href="/staff" className="block py-1 px-2 rounded hover:bg-gray-700">(as if staff)</a>
+      </div>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
